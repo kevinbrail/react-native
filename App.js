3e07b48ec01569191368e12bbcdd194d2f669ae3
Kevin Brail <kevinabrail@gmail.com>
2019-03-20 11:34:04 -0400

    initial test app

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>KEVIN IS AWESOME!</Text>
+        <Text>BUT SAM GETS THE CHEESE!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    color: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
